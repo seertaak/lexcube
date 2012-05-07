af1279a9dc85bcf1e34eb621ac8383dff51740c9
Martin Percossi <martin@percossi.com>
2012-05-07 19:05:44 +0200

    More work on login.

--- app.js
+++ app.js
@@ -5,7 +5,107 @@
 
 var express = require('express'), 
     routes = require('./routes'),
-    stylus = require('stylus');
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    mongoose = require('mongoose'),
+    stylus = require('stylus'),
+    Schema = mongoose.Schema;
+
+var users = [
+  { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' },
+  { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unkown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+  }
+));
+
+
+
+
+/*
+var UserSchema = new Schema({
+  username: String,
+  password: String,
+  created: {type: Date, default: Date.now}
+});
+
+mongoose.connect('mongodb://localhost/lexcube');
+mongoose.model('User', UserSchema);
+
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username, password: password }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Unknown user' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Invalid password' });
+      }
+      return done(null, user);
+    });
+  }
+));
+*/
 
 var app = module.exports = express.createServer();
 
@@ -16,8 +116,12 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.logger());
+  app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.session({ secret: 'aguilarobbe' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(stylus.middleware({ src: __dirname + '/public' }));
   app.use(express.static(__dirname + '/public'));
@@ -36,6 +140,17 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('/lookup', routes.lookup);
 app.get('/login', routes.login);
+/*
+app.post('/login', 
+  passport.authenticate('local', { successRedirect: '/',
+    failureRedirect: '/login', failureFlash: true })
+);
+*/
+
+app.post('/login', function(req, res) {
+    console.log("req.query=" + JSON.stringify(req.query));
+});
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
