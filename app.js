b26133290f0d8a93c015d29c61a658f0883f28a0
Martin Percossi <mpercossi@gmail.com>
2012-05-09 11:14:17 +0200

    Separating functionality into modules.

--- app.js
+++ app.js
@@ -1,57 +1,23 @@
 
-/**
- * Module dependencies.
- */
-
+// Module dependencies.
+// 
 var express = require('express'), 
     routes = require('./routes'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     mongoose = require('mongoose'),
     stylus = require('stylus'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    db = require('./lib/db'),
+    passp = require('./lib/auth');
 
-var UserSchema = new Schema({
-  username: {type: String, index: true},
-  hashed_password: String,
-  created: {type: Date, default: Date.now}
-});
-
-mongoose.connect('mongodb://localhost/lexcube');
-
-var User = mongoose.model('User', UserSchema);
-
-passport.serializeUser(function(user, done) {
-  done(null, user.username);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findOne(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) {
-        return done(err);
-      } else if (!user) {
-        return done(null, false, { message: 'Unknown user' });
-      } else if (user.password !== password) {
-        return done(null, false, { message: 'Invalid password' });
-      } else {
-        return done(null, user);
-      }
-    });
-  }
-));
+// Initialize the db and models
+db.init();
 
 var app = module.exports = express.createServer();
 
 // Configuration
 //
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -80,37 +46,37 @@ app.get('/', routes.index);
 app.get('/lookup', routes.lookup);
 app.get('/login', routes.login);
 app.post('/login', function(req, res) {
-    if ('createAcct' in req.body) {
-      //console.log("createAcct: " + req.body.username + ":" + req.body.password);
-      User.find({username: req.body.username}, function (err, docs) {
-        if (docs.length) {
-          // we can't use this username, as it's already taken: send a flash
-          // message to the user to this effect.
-          //console.log("MPD: " + JSON.stringify(docs));
-          req.flash('error', 'User already exists; please use a different username.');
-          res.redirect('/login');
-        } else {
-          //console.log("MPD: No such user");
-          var user = new User();
-          user.username = req.body.username;
-          user.password = req.body.password;
-          user.save(function (err) {
-            if (err) {
-              //console.log("Error while trying to save user: " + JSON.stringify(user) + ":" + err);
-              req.flash('error', 'Oops, there was a problem: please try again!');
-              res.redirect('/login');
-            } else {
-              passport.authenticate('local', { successRedirect: '/',
-                failureRedirect: '/login', failureFlash: true })(req, res);
-            }
-          });
-        }
-      });
-    } else {
-      //console.log("MPD: attempting to log in.");
-      passport.authenticate('local', { successRedirect: '/',
-        failureRedirect: '/login', failureFlash: true })(req, res);
-    }
+  if ('createAcct' in req.body) {
+    //console.log("createAcct: " + req.body.username + ":" + req.body.password);
+    db.User.find({username: req.body.username}, function (err, docs) {
+      if (docs.length) {
+        // we can't use this username, as it's already taken: send a flash
+        // message to the user to this effect.
+        //console.log("MPD: " + JSON.stringify(docs));
+        req.flash('error', 'User already exists; please use a different username.');
+        res.redirect('/login');
+      } else {
+        //console.log("MPD: No such user");
+        var user = new db.User();
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.save(function (err) {
+          if (err) {
+            //console.log("Error while trying to save user: " + JSON.stringify(user) + ":" + err);
+            req.flash('error', 'Oops, there was a problem: please try again!');
+            res.redirect('/login');
+          } else {
+            passport.authenticate('local', { successRedirect: '/',
+              failureRedirect: '/login', failureFlash: true })(req, res);
+          }
+        });
+      }
+    });
+  } else {
+    //console.log("MPD: attempting to log in.");
+    passport.authenticate('local', { successRedirect: '/',
+      failureRedirect: '/login', failureFlash: true })(req, res);
+  }
 });
 
 
